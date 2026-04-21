5cdd3090f5cfc34e81893c53d9cdb5e788983654
Edson-Emanoel <edson.emanoelofc@gmail.com>
2026-04-21 12:40:51 -0300

    feat&docs: adicionei uma informação importantíssima no Readme e terminei o vídeo 31 que é o de tailwind

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.guest-layout subtitle="{{ empty($subtitle) ? '' : $subtitle }}">
     <p class="text-3xl text-center text-blue-600 mt-10">
-        <i class="fa-solid fa-thumbs-up">BATATA</i>
+        <i class="fa-solid fa-thumbs-up"></i> A nossa view
     </p>
 </x-layouts.guest-layout>
